b3c8bc55a212b44f48857ef42ac54a8ca9d87539
Diego Szychowski <diegos@exo.com.ar>
2024-09-29 10:29:22 -0300

    chore: update version to 0.0.2000000002003

--- version.go
+++ version.go
@@ -3,4 +3,4 @@ package main
 // Version can be set at link time to override debug.BuildInfo.Main.Version,
 // which is "(devel)" when building from within the module. See
 // golang.org/issue/29814 and golang.org/issue/29228.
-var Version = "0.0.2000000002002"
+var Version = "0.0.2000000002003"
